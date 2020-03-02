d2e2faf01eaf696d411f8171edec9112490e66f8
iyo0323 <40633134+iyo0323@users.noreply.github.com>
2020-03-02 12:06:57 +0900

    Update Django_API.py

--- Django_API.py
+++ Django_API.py
@@ -13,6 +13,51 @@ urlpatterns = [
     path('{aaa}/<int:pk>/update/', views.{Aaa}Update.as_view()),
 ]
 
+
+
 #----------------------------------------------------------------------------------
-# 
+# {AppName}\views\XXX.py
 #----------------------------------------------------------------------------------
+from rest_framework import generics
+from django_filters import rest_framework as filters
+from {AppName}.serializer import {Aaa}Serializer
+import django_filters
+
+class {Aaa}IdFilter(filters.FilterSet):
+    # id　→　DB上の実体Field名
+    # {aaa}_id　→　Django側で自命名の名前
+    # urlの最後に「?{aaa}_id=1」と指定すれば、id=1のデータだけ抽出になる
+    {aaa}_id = django_filters.NumberFilter(field_name="id", lookup_expr='exact')
+
+    class Meta:
+        model = {Table_Name}
+        fields = ['{aaa}_id']
+
+
+class {Aaa}List(generics.ListCreateAPIView):
+    """
+    REST API (Select List. You need to set filter)
+    """
+    queryset = {Table_Name}.objects.all()
+    serializer_class = {Aaa}Serializer
+
+    filter_class = {Aaa}IdFilter
+    # permission_classes = [IsAdminUser]
+
+
+class {Aaa}(generics.RetrieveAPIView):
+    """
+    REST API (Select One Entry. You need to set <pk> in urls file)
+    """
+    queryset = {Table_Name}.objects.all()
+    serializer_class = {Aaa}Serializer
+
+
+class {Aaa}Update(generics.RetrieveUpdateAPIView):
+    """
+    REST API (Update. You need to set <pk> in urls file)
+    """
+    queryset = {Table_Name}.objects.all()
+    serializer_class = {Aaa}Serializer
+
+
