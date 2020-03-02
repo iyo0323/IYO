f0be2ea83472c1f636639c7b5a0cac4bd31a7d69
iyo0323 <40633134+iyo0323@users.noreply.github.com>
2020-03-02 12:18:13 +0900

    Update Django_API.py

--- Django_API.py
+++ Django_API.py
@@ -1,7 +1,17 @@
+
+#----------------------------------------------------------------------------------
+# Def
+#----------------------------------------------------------------------------------
+#{aaa}: Objectのコードネーム（ex. customer）
+#{Aaa}: Objectのコードネーム（ex. Customer）
+#{AppName}: APPの名称（ex. customermanage）
+#{Table_Name}: DB上Tableの物理名（ex. master_customer）
+#{TableField}: DB上Fieldの物理名（ex. birthday, sex ... etc）
+
+
 #----------------------------------------------------------------------------------
 # {AppName}\urls.py
 #----------------------------------------------------------------------------------
-#{aaa},{Aaa}
 from django.urls import path
 from {AppName} import views
 urlpatterns = [
