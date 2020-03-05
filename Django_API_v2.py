e66fc6379dc4210f22ad668cba601f2f1c33fabb
iyo0323 <40633134+iyo0323@users.noreply.github.com>
2020-03-05 12:38:56 +0900

    Update Django_API_v2.py

--- Django_API_v2.py
+++ Django_API_v2.py
@@ -17,3 +17,61 @@ from django.apps import AppConfig
 class {AppName}Config(AppConfig):
     name = '{AppName}'
 
+
+
+#----------------------------------------------------------------------------------
+# {AppName}\urls.py
+#----------------------------------------------------------------------------------
+from django.conf.urls import url
+from {AppName} import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'{aaa}s', views.{Aaa}ViewSet)
+
+
+
+#----------------------------------------------------------------------------------
+# {AppName}\views\__init__.py
+#----------------------------------------------------------------------------------
+from .{aaa} import {Aaa}ViewSet
+
+
+
+
+#----------------------------------------------------------------------------------
+# {AppName}\views\base.py
+#----------------------------------------------------------------------------------
+from .mixin import {Bbb}CreateModelMixin
+from .mixin import {Bbb}UpdateModelMixin
+from rest_framework import status, mixins
+from rest_framework.viewsets import GenericViewSet
+
+
+
+class {Bbb}ModelViewSet({Bbb}CreateModelMixin,
+                       mixins.RetrieveModelMixin,
+                       {Bbb}UpdateModelMixin,
+                       mixins.DestroyModelMixin,
+                       mixins.ListModelMixin,
+                       GenericViewSet):
+    """
+    A viewset that provides default `create()`, `retrieve()`, `update()`,
+    `partial_update()`, `destroy()` and `list()` actions.
+    """
+
+    pass
+
+
+
+#----------------------------------------------------------------------------------
+# {AppName}\views\mixins.py
+#----------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
